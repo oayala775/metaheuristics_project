6779db0309d1ee237f27c196c147b9b8ca9caaed
omar Linux Victus <oayala775@gmail.com>
2025-03-27 07:20:22 -0600

    Changing PR to work as usual

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
 from test_cases.mazes import mazes
 from algorithms.Enhanced_ACO import EnhancedACO
+from algorithms.ACO import ACO
+from test_cases.maze import Maze
 # from algorithms.ACO import ACO
 
 def main():
     grid = mazes[4]
-    algorithm = EnhancedACO(50, 150, grid.maze, grid.start, grid.goal)
-    # algorithm = ACO(50, 150, grid.maze, grid.start, grid.goal)
+    algorithm = ACO(50, 150, grid.maze, grid.start, grid.goal)
     best_path, best_lenght = algorithm.ant_colony()
-
-    grid.plot_maze(best_path)
+    grid.best_path, grid.best_length = best_path, best_lenght
+    grid.plot_maze()
     
 
 if __name__ == "__main__":
