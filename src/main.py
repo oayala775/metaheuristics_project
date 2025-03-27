28b4878cd5e1ced98dc434257e6d654d12d4d31c
Omar HP Windows <oayala775@gmail.com>
2025-03-27 17:50:43 -0600

    Correcting mispells lenght -> length

--- src/main.py
+++ src/main.py
@@ -2,12 +2,13 @@ from test_cases.mazes import mazes
 from algorithms.Enhanced_ACO import EnhancedACO
 from algorithms.ACO import ACO
 from test_cases.maze import Maze
+from algorithms.ABC import ArtificialBeeColony as ABC
 # from algorithms.ACO import ACO
 
 def main():
-    grid = mazes[4]
-    algorithm = ACO(50, 150, grid.maze, grid.start, grid.goal)
-    best_path, best_lenght = algorithm.ant_colony()
+    grid = mazes[0]
+    algorithm = ABC(grid.maze, grid.start, grid.goal)
+    best_path, best_lenght = algorithm.optimize()
     grid.best_path, grid.best_length = best_path, best_lenght
     grid.plot_maze()
     
