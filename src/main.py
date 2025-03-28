b05cea0ba800301c3e5e22045515f8641da8d5e8
Omar HP Windows <oayala775@gmail.com>
2025-03-28 16:07:01 -0600

    Deleting commented sections in main.py and metrics.py

--- src/main.py
+++ src/main.py
@@ -12,20 +12,7 @@ def main():
     ]
 
     for algorithm in algorithms:
-        metrics.evaluate_algorithm(algorithm[0], algorithm[1], grid)
-
-# from test_cases.mazes import mazes
-# from test_cases.maze import Maze
-# from algorithms.ABC import ArtificialBeeColony as ABC
-# from algorithms.ACO import ACO
-# 
-# def main():
-    # grid = mazes[0]
-    # algorithm = ABC(grid.maze, grid.start, grid.goal)
-    # best_path, best_lenght = algorithm.optimize()
-    # grid.best_path, grid.best_length = best_path, best_lenght
-    # grid.plot_maze()
-    
+        metrics.evaluate_algorithms(algorithm[0], algorithm[1], grid)    
 
 if __name__ == "__main__":
     main()
--- src/metrics/metrics.py
+++ src/metrics/metrics.py
@@ -1,6 +1,4 @@
 from sklearn.metrics import mean_squared_error
-# from skimage.metrics import peak_signal_noise_ratio
-# from skimage.metrics import structural_similarity
 from test_cases.maze import Maze
 from time import time
 import numpy as np
@@ -26,16 +24,4 @@ class Metrics:
         if plot_grid != None:
             plot_grid.best_path, plot_grid.best_length = best_path, best_lenght
             plot_grid.plot_maze()
-    
-    # def mse(self, true_solution, predicted_solution) -> float: 
-        # return mean_squared_error(true_solution, predicted_solution)
-    
-    # def psnr(self, true_image:np.ndarray, test_image:np.ndarray) -> float:
-        # return peak_signal_noise_ratio(true_image, test_image)
-    # 
-    # def ssim(self, true_image:np.ndarray, test_image:np.ndarray) -> float:
-        # return structural_similarity(true_image, test_image)
-    
-    # def std(self):
-    #     return np.std()
 
