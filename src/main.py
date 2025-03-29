40b79c7b731347e0aee0a8b3e2ade557e58bb279
omar Linux Victus <oayala775@gmail.com>
2025-03-29 13:12:31 -0600

    Minimized main.py

--- src/main.py
+++ src/main.py
@@ -1,24 +1,10 @@
-from test_cases.mazes import mazes
-from algorithms.Enhanced_ACO import EnhancedACO as EACO
-from algorithms.ACO import ACO
-from algorithms.ABC import ArtificialBeeColony as ABC
 from metrics.metrics import Metrics
-from algorithms.PSO import PSO
-
 
 
 def main():
-    grid = mazes[1]
     metrics = Metrics()
-    algorithms = [
-        (ACO(grid.maze, grid.start, grid.goal), "ACO"),
-        (EACO(grid.maze, grid.start, grid.goal), "EACO"),
-        (PSO(grid.maze, grid.start, grid.goal), "PSO"),
-        (ABC(grid.maze, grid.start, grid.goal), "ABC"),
-    ]
 
-    for algorithm in algorithms:
-        metrics.evaluate_algorithm(algorithm[0], algorithm[1], grid)
+    metrics.start()
 
 
 if __name__ == "__main__":
