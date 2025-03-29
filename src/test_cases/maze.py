883fd3c4bb7772e9fdc6908392f6940238adc95d
Omar HP Windows <oayala775@gmail.com>
2025-03-28 18:58:34 -0600

    Applied PEP8 standards to increase legibility and also removed unused file

--- src/test_cases/maze.py
+++ src/test_cases/maze.py
@@ -1,10 +1,10 @@
 from matplotlib.colors import ListedColormap
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import numpy as np
 
 
 class Maze:
-    def __init__(self, maze:np.ndarray, start: tuple, goal: tuple):
+    def __init__(self, maze: np.ndarray, start: tuple, goal: tuple):
         """Start and goal tuples must be in (y,x) form, in order for the code to work properly"""
         self.cmap = ListedColormap(['white', 'black'])
 
@@ -15,70 +15,69 @@ class Maze:
         self.__best_path = None
         self.__best_length = None
 
-
         self.__x_length, self.__y_length = maze.shape
 
     @property
     def maze(self) -> np.ndarray:
         return self.__maze
-    
-    @maze.setter 
-    def maze(self, maze:np.ndarray) -> None:
+
+    @maze.setter
+    def maze(self, maze: np.ndarray) -> None:
         self.__maze = maze
 
     @property
     def start(self) -> tuple:
         return self.__start
-    
+
     @start.setter
-    def start(self, start:tuple) -> None:
+    def start(self, start: tuple) -> None:
         self.__start = start
-    
+
     @property
     def goal(self) -> tuple:
         return self.__goal
 
     @goal.setter
-    def goal(self, goal:tuple) -> None:
+    def goal(self, goal: tuple) -> None:
         self.__goal = goal
-    
+
     @property
     def best_path(self) -> list[tuple]:
         return self.__best_path
-    
+
     @best_path.setter
-    def best_path(self, path:list[tuple]) -> None:
+    def best_path(self, path: list[tuple]) -> None:
         self.__best_path = path
-    
+
     @property
     def best_length(self) -> float:
         return self.__best_length
 
     @best_length.setter
-    def best_length(self, length:float) -> None:
+    def best_length(self, length: float) -> None:
         self.__best_length = length
-    
+
     @property
     def x_length(self) -> int:
         return self.__x_length
-    
+
     @x_length.setter
-    def x_length(self, x_len:int) -> None:
+    def x_length(self, x_len: int) -> None:
         self.__x_length = x_len
-    
+
     @property
     def y_length(self) -> int:
         return self.__y_length
-    
+
     @y_length.setter
-    def y_length(self, y_len:int) -> None:
+    def y_length(self, y_len: int) -> None:
         self.__y_length = y_len
-    
+
     def plot_maze(self, algorithm_name: str) -> None:
         if not self.best_length and not self.best_path:
             print("Best length or Best path are not defined")
             return
-        
+
         fig, ax = plt.subplots(figsize=(6, 6))
         ax.imshow(self.maze, cmap=self.cmap)
 
@@ -87,18 +86,22 @@ class Maze:
         ax.set_yticks(np.arange(-0.5, self.x_length, 1), minor=True)
         ax.grid(which="minor", color="gray", linestyle='-', linewidth=0.5)
         ax.tick_params(which="minor", size=0)
-        ax.tick_params(which="both", bottom=False, left=False, 
-                    labelbottom=False, labelleft=False)
-        
+        ax.tick_params(which="both", bottom=False, left=False,
+                       labelbottom=False, labelleft=False)
+
         if self.best_path:
             x = [pos[1] for pos in self.best_path]  # Columnas
             y = [pos[0] for pos in self.best_path]  # Filas
 
-            ax.plot(x, y, color='red', linewidth=2, marker='o', markersize=4, label='Camino óptimo')
-            ax.plot(self.goal[1],self.goal[0], marker='x', markersize=10, color='green', label='Objetivo')
-            ax.plot(self.start[1],self.start[0], marker='o', markersize=10, color='blue', label='Inicio')
+            ax.plot(x, y, color='red', linewidth=2, marker='o',
+                    markersize=4, label='Camino óptimo')
+            ax.plot(self.goal[1], self.goal[0], marker='x',
+                    markersize=10, color='green', label='Objetivo')
+            ax.plot(self.start[1], self.start[0], marker='o',
+                    markersize=10, color='blue', label='Inicio')
             ax.legend()
 
-        plt.title(f"Algoritmo: {algorithm_name} \nLaberinto {self.x_length}x{self.y_length} - Celdas Pequeñas")
+        plt.title(
+            f"Algoritmo: {algorithm_name} \nLaberinto {self.x_length}x{self.y_length} - Celdas Pequeñas")
         plt.tight_layout()  # Ajustar layout para que quede compacto
         plt.show()
