2367200a350a5693975b39df3c333c612cd8f7ba
Omar HP Windows <oayala775@gmail.com>
2025-03-28 19:02:26 -0600

    Removed tools folder as all of its files are already written in Algorithm class

--- src/tools/euclidean_distance.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def euclidean_distance(position1: tuple[int,int], position2: tuple[int,int]) -> float:
-    """
-        Mide la distancia euclideana de un punto A a un punto B
-    """
-    return np.sqrt((position1[0] - position2[0])**2 + (position1[1] - position2[1])**2)
